f1e5e9a92b283ba705be2fdce54796b70b2a89e2
Joshua Ajorgbor <joshuaajorgbor@gmail.com>
2023-01-05 10:41:00 +0100

    converted portal index page to next.js

--- apps/portal/pages/_app.js
+++ apps/portal/pages/_app.js
@@ -1,10 +1,32 @@
 import Head from 'next/head'
-import './styles.css'
+import Script from 'next/head'
+import '../public/assets/css/style.css'
+import '../public/assets/css/green.css'
 function CustomApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Welcome to portal!</title>
+        {/* Basic Page Needs */}
+        <title>Portal — Address Data</title>
+        <meta
+          name="description"
+          content="Deploy, configure and hyper-accurate Nigeria Address Validation Tool."
+        />
+        <meta name="founder" content="Abdulhaqq Sule, writeme@abdulhaqqsule.com" />
+        <meta name="contributors" content="https://addressdata.ng/contributors" />
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+        {/* Favicon */}
+        <link
+          rel="shortcut icon"
+          href="../public/images/favicon.png"
+          type="image/x-icon"
+        />
+
+
       </Head>
       <main className="app">
         <Component {...pageProps} />
